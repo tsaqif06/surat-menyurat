c9b10aec9f6760fd0b340080ac66df29cdd0b5da
Ahmad Tsaqif Al Arifin <tsaqifcakep87@gmail.com>
2024-08-21 20:48:45 +0700

    update sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -22,73 +22,79 @@
         </li>
 
         <li class="menu-header small text-uppercase">
-            <span class="menu-header-text">{{ __('menu.header.main_menu') }}</span>
-          </li>
-          <li class="menu-item">
+            <span class="menu-header-text">Master Data</span>
+        </li>
+
+        {{--  <li class="menu-item">
             <a href="app-email.html" class="menu-link">
-              <i class="menu-icon tf-icons bx bx-envelope"></i>
-              <div class="text-truncate" data-i18n="Email">User</div>
+                <i class="menu-icon tf-icons bx bx-envelope"></i>
+                <div class="text-truncate" data-i18n="Email">User</div>
             </a>
-          </li>
-          <li class="menu-item">
+        </li>  --}}
+        <li class="menu-item">
             <a href="{{ route('jabatan.index') }}" class="menu-link">
-              <i class="menu-icon tf-icons bx bx-chat"></i>
-              <div class="text-truncate" data-i18n="Chat">Jabatan</div>
+                <i class="menu-icon tf-icons bx bx-chat"></i>
+                <div class="text-truncate" data-i18n="Chat">Jabatan</div>
             </a>
-          </li>
-          <li class="menu-item">
+        </li>
+        <li class="menu-item">
             <a href="{{ route('bagian.index') }}" class="menu-link">
-              <i class="menu-icon tf-icons bx bx-calendar"></i>
-              <div class="text-truncate" data-i18n="Calendar">Bagian</div>
+                <i class="menu-icon tf-icons bx bx-calendar"></i>
+                <div class="text-truncate" data-i18n="Calendar">Bagian</div>
             </a>
-          </li>
-          <li class="menu-item">
+        </li>
+        <li class="menu-item">
             <a href="{{ route('jenis.index') }}" class="menu-link">
-              <i class="menu-icon tf-icons bx bx-grid"></i>
-              <div class="text-truncate" data-i18n="Kanban">Jenis Surat</div>
+                <i class="menu-icon tf-icons bx bx-grid"></i>
+                <div class="text-truncate" data-i18n="Kanban">Jenis Surat</div>
             </a>
-          </li>
-          <li class="menu-item">
+        </li>
+        <li class="menu-item">
             <a href="{{ route('ruang.index') }}" class="menu-link">
-              <i class="menu-icon tf-icons bx bx-grid"></i>
-              <div class="text-truncate" data-i18n="Kanban">Ruang Penyimpanan</div>
+                <i class="menu-icon tf-icons bx bx-grid"></i>
+                <div class="text-truncate" data-i18n="Kanban">Ruang Penyimpanan</div>
             </a>
-          </li>
-          <li class="menu-item">
+        </li>
+        <li class="menu-item">
             <a href="{{ route('relasi.index') }}" class="menu-link">
-              <i class="menu-icon tf-icons bx bx-grid"></i>
-              <div class="text-truncate" data-i18n="Kanban">Perusahaan/Relasi</div>
+                <i class="menu-icon tf-icons bx bx-grid"></i>
+                <div class="text-truncate" data-i18n="Kanban">Perusahaan/Relasi</div>
             </a>
-          </li>
-          <li class="menu-item">
+        </li>
+
+        <li class="menu-header small text-uppercase">
+            <span class="menu-header-text">{{ __('menu.header.main_menu') }}</span>
+        </li>
+
+        <li class="menu-item">
             <a href="{{ route('surat.index') }}" class="menu-link">
-              <i class="menu-icon tf-icons bx bx-grid"></i>
-              <div class="text-truncate" data-i18n="Kanban">Surat</div>
+                <i class="menu-icon tf-icons bx bx-grid"></i>
+                <div class="text-truncate" data-i18n="Kanban">Surat</div>
             </a>
-          </li>
-          <li class="menu-item">
+        </li>
+        <li class="menu-item">
             <a href="{{ route('disposisi.index') }}" class="menu-link">
-              <i class="menu-icon tf-icons bx bx-grid"></i>
-              <div class="text-truncate" data-i18n="Kanban">Disposisi SM</div>
+                <i class="menu-icon tf-icons bx bx-grid"></i>
+                <div class="text-truncate" data-i18n="Kanban">Disposisi SM</div>
             </a>
-          </li>
-          <li class="menu-item">
+        </li>
+        <li class="menu-item">
             <a href="{{ route('approve.index') }}" class="menu-link">
-              <i class="menu-icon tf-icons bx bx-grid"></i>
-              <div class="text-truncate" data-i18n="Kanban">Persetujuan</div>
+                <i class="menu-icon tf-icons bx bx-grid"></i>
+                <div class="text-truncate" data-i18n="Kanban">Persetujuan</div>
             </a>
-          </li>
-          <li class="menu-item">
+        </li>
+        <li class="menu-item">
             <a href="app-kanban.html" class="menu-link">
-              <i class="menu-icon tf-icons bx bx-grid"></i>
-              <div class="text-truncate" data-i18n="Kanban">Arsip</div>
+                <i class="menu-icon tf-icons bx bx-grid"></i>
+                <div class="text-truncate" data-i18n="Kanban">Arsip</div>
             </a>
-          </li>
-          <li class="menu-item">
+        </li>
+        <li class="menu-item">
             <a href="app-kanban.html" class="menu-link">
-              <i class="menu-icon tf-icons bx bx-grid"></i>
-              <div class="text-truncate" data-i18n="Kanban">Laporan</div>
+                <i class="menu-icon tf-icons bx bx-grid"></i>
+                <div class="text-truncate" data-i18n="Kanban">Laporan</div>
             </a>
-          </li>
+        </li>
     </ul>
 </aside>
